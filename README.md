f92b4361c06a12df4b8a32634c2cf59a72901828
thinknathan <github@thinknathan.com>
2024-02-23 17:10:43 -0800

    Add new preload tag functionality

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 <img src="_docs/def-sw.png" alt="Tool for Defold: Def-SW">
 
-# Defold Service Worker Generator
+# HTML5 Build Optimizer for Defold
 
 [![CI](https://github.com/thinknathan/def-sw/actions/workflows/ci.yml/badge.svg)](https://github.com/thinknathan/def-sw/actions/workflows/ci.yml)
 
-Generates a service worker for web games exported from [Defold](https://defold.com/).
+Adds loading and caching optimizations to HTML5 web games exported from [Defold](https://defold.com/).
+
+1. Minifies HTML and JS to reduce file size and speed loading
+2. Adds preload meta tags to speed up first boot
+3. Generates a service worker to preload and cache assets
 
 ## Install
 
 1. Install [Nodejs](https://nodejs.org/en) or equivalent
 
-2. Clone this project
+2. Clone this project (or fork it in Github)
    `git clone https://github.com/thinknathan/def-sw`
 
 3. Install dependencies
@@ -21,9 +25,14 @@ Generates a service worker for web games exported from [Defold](https://defold.c
 ## Usage
 
 1. Build your HTML5 export from Defold
-2. Copy your game files into the `copy-game-here` folder (exclude `report.html` and similar)
-3. (Optional) Run `yarn minify` or `npm run minify` to reduce the size of vanilla files `index.html` and `dmloader.js`
-4. Run `yarn build` or `npm run build` to generate the service worker
+2. Copy your game files into the `copy-game-here` folder (don't include the build report: exclude `report.html` and similar)
+3. Run `yarn preload` or `npm run preload` to add preload meta tags
+4. (Optional) Run `yarn minify` or `npm run minify` to reduce the size of `index.html` and `dmloader.js`
+5. Run `yarn sw` or `npm run sw` to generate the service worker
 
-- Done! The files will be generated in the `copy-game-here` folder
+- Done! We've completed the modifications to your game in the `copy-game-here` folder
 - Upload all the files inside `copy-game-here` to distribute your game
+
+## Other potential optimizations
+
+Since Defold's usage of emscripten lags behind the latest release, you may be able to gain a speed/file size improvement by downloading the latest version of [wasm-opt](https://github.com/WebAssembly/binaryen) and running it on your wasm binary.
---
+++ tools/add-preload-tags.cjs
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const path = require('path');
+const preloadTags = [];
+
+/**
+ * @param {string} absolutePath
+ * @param {string} baseDirectory
+ * @returns string
+ */
+const convertToRelativePath = (absolutePath, baseDirectory) => {
+	const relativePath = path.relative(baseDirectory, absolutePath);
+	return relativePath.replace(/\\/g, '/'); // Convert backslashes to forward slashes for compatibility
+};
+
+/**
+ * @param {string} directory
+ * @param {string[]} blockList
+ */
+const traverseDirectory = (directory, blockList) => {
+	const files = fs.readdirSync(directory);
+
+	files.forEach((file) => {
+		const filePath = path.join(directory, file);
+		if (fs.statSync(filePath).isDirectory()) {
+			traverseDirectory(filePath, blockList);
+		} else {
+			if (!blockList.some((entry) => filePath.includes(entry))) {
+				if (filePath.includes('dmloader.js')) {
+					preloadTags.push(
+						`  <link rel="preload" href="${convertToRelativePath(filePath, 'copy-game-here')}" as="script" />\n`,
+					);
+				} else {
+					preloadTags.push(
+						`  <link rel="preload" href="${convertToRelativePath(filePath, 'copy-game-here')}" as="fetch" crossorigin="*" />\n`,
+					);
+				}
+			}
+		}
+	});
+};
+
+const main = () => {
+	const currentDirectory = process.cwd();
+	const copyGameDirectory = path.join(currentDirectory, 'copy-game-here');
+	const htmlFilePath = path.join(copyGameDirectory, 'index.html');
+	const blockList = [
+		'node_modules',
+		'Thumbs.db',
+		'ehthumbs.db',
+		'Desktop.ini',
+		'$RECYCLE.BIN/',
+		'.cab',
+		'.msi',
+		'.msm',
+		'.msp',
+		'.lnk',
+		'.DS_Store',
+		'.AppleDouble',
+		'.LSOverride',
+		'._',
+		'.DocumentRevisions-V100',
+		'.fseventsd',
+		'.Spotlight-V100',
+		'.TemporaryItems',
+		'.Trashes',
+		'.VolumeIcon.icns',
+		'.AppleDB',
+		'.AppleDesktop',
+		'Network Trash Folder',
+		'Temporary Items',
+		'.apdisk',
+		'.html',
+		'.gitkeep',
+		'robots.txt',
+		'humans.txt',
+		'browserconfig.xml',
+		'apple-icon',
+		'apple-splash',
+		'mstile-icon',
+		'manifest-icon',
+		'.map',
+		'service-worker.js',
+		'load-sw.js',
+		'workbox-',
+		'_asmjs.js',
+	];
+
+	traverseDirectory(copyGameDirectory, blockList);
+
+	fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+		if (err) {
+			console.error(`Error reading file: ${err}`);
+			return;
+		}
+
+		if (preloadTags.length === 0) {
+			console.log('No files to preload.');
+			return;
+		}
+
+		if (data.includes('<link rel="preload"')) {
+			console.log('Preload tag already exists.');
+			return;
+		}
+
+		const modifiedData = data.replace(
+			'</title>',
+			'</title>\n' + preloadTags.join(''),
+		);
+
+		fs.writeFile(htmlFilePath, modifiedData, 'utf8', (writeErr) => {
+			if (writeErr) {
+				console.error(`Error writing to file: ${writeErr}`);
+			} else {
+				console.log('Preload tags inserted successfully.');
+			}
+		});
+	});
+};
+
+main();
